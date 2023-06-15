86badc5cba6c6012b755cb1331f369d366addb6d
ClaudioSandro <u202215099@upc.edu.pe>
2023-06-14 20:23:51 -0500

    cambio en HU13

--- AcceptanceTest en JavaScript/AcceptanceTestHU13.js
+++ AcceptanceTest en JavaScript/AcceptanceTestHU13.js
@@ -3,12 +3,15 @@ const { Given, And, When, Then } = require('cucumber');
 Scenario('Agendar una cita para una asistencia', function () {
     Given('un usuario desea tener una cita con un profesor virtual para mejorar su rendimiento.', function () { });
     And('el usuario selecciona el apartado de "Agendar cita".', function () { });
-    When('el usuario rellene sus datos en los campos obligatorios, seleccione el horario  y hace click en "Registrar cita".', function () { });
+    When('el usuario seleccione a un profesor, rellene el nombre, seleccione el horario y hace clic en "Registrar cita".', function () { });
     Then('el sistema valida que haya rellenado los campos correctamente y se le verifica vía correo electrónico que su cita ha sido registrada.', function () { });
 });
 /*
 Input:
-- Clic en agendar cita
+- profesor
+- nombre 
+- horario
+- registrar cita
 Output:
-- Verificación de cita
+- verificación de cita
 */
